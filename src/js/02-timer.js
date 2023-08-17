7fad79fb92479b472f47425beb17f3ae8a34d537
ravkamaria <hevkomaria1@gmail.com>
2023-08-18 02:01:18 +0300

    Task 02 in progress

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,25 +1,70 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from 'notiflix';
-const flatpickr = require("flatpickr");
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const flatpickr = require('flatpickr');
 
-const btnStart = document.querySelector("button[data-start]");
-btnStart.addEventListener("click", handlerStart);
+const btnStart = document.querySelector('button[data-start]');
+const input = document.querySelector("input");
+const timerValue = {
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+btnStart.addEventListener('click', handlerStart);
+btnStart.setAttribute('disabled', '');
 
-function handlerStart(){
-    
-    btnStart.setAttribute('disabled', '');
+const now = new Date();
+let timer = 86369724;
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const date = new Date(selectedDates);
+    timer = date - now;
+    if (timer <= 0) {
+      Notify.failure('Please choose a date in the future');
+    }
+    btnStart.removeAttribute('disabled');
+    // console.log(timer)
+    return timer;
+  },
+});
+
+console.log(timer)
+function handlerStart() {
+ const startTimer = setInterval(() => {
+  timer -= 1000;
+    const timerArr = convertMs(timer);
+    console.log(timer)
+    addLeadingZero(timerArr);
+    if(timer < 1000){
+    clearInterval(startTimer);
+  }
+  }, 1000);
+  btnStart.setAttribute('disabled', '');
+  input.setAttribute('disabled', '');
 }
 
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-flatpickr("#datetime-picker",
-{
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  });
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero({ days, hours, minutes, seconds }) {
+  timerValue.days.textContent = days;
+  timerValue.hours.textContent = hours.toString().padStart(2, "0");
+  timerValue.minutes.textContent = minutes.toString().padStart(2, "0");
+  timerValue.seconds.textContent = seconds.toString().padStart(2, "0");
+}
