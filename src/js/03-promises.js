59239a6ea54a78a7f7a648137905f7ad49a98748
ravkamaria <hevkomaria1@gmail.com>
2023-08-18 03:07:33 +0300

    commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,14 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// Notiflix.Notify.success('Sol lucet omnibus');
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+const form = document.querySelector(".form");
+const input = document.querySelector("input")
+console.log(form);
+console.dir(input.name="delay");
+
+
